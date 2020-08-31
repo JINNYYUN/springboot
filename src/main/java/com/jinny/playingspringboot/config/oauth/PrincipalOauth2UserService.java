6bc82d09e0590fc2d7d518acd7b63a22c63a0452
YUNJIMYEONG <yjim3440@gmail.com>
2020-08-31 23:20:50 +0900

    [edit/security] 네이버 로그인 추가

--- src/main/java/com/jinny/playingspringboot/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/jinny/playingspringboot/config/oauth/PrincipalOauth2UserService.java
@@ -1,6 +1,9 @@
 package com.jinny.playingspringboot.config.oauth;
 
 import com.jinny.playingspringboot.config.auth.PrincipalDetails;
+import com.jinny.playingspringboot.config.oauth.provider.GoogleUserInfo;
+import com.jinny.playingspringboot.config.oauth.provider.NaverUserInfo;
+import com.jinny.playingspringboot.config.oauth.provider.OAuth2UserInfo;
 import com.jinny.playingspringboot.model.User;
 import com.jinny.playingspringboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,9 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -29,26 +35,40 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         System.out.println("userRequest accessToken : " + userRequest.getAccessToken());
 
         // 2. google의 회원 프로필 조회
-        //userRequest정보 -> loadUser함수 호출 -> 구글로부터 회원프로필을 받아준다.
+        //userRequest정보 -> loadUser함수 호출 -> 회원프로필을 받아준다.
         OAuth2User oAuth2User = super.loadUser(userRequest);
         System.out.println("oAuth2User : " + oAuth2User);// token을 통해 응답받은 회원정보
         System.out.println("getAttribute : " + oAuth2User.getAttributes());
 
         //3. 받은 정보를 토대로, 회원가입을 진행
-        String provider = userRequest.getClientRegistration().getClientId(); //google
-        String providerId = oAuth2User.getAttribute("sub");
+        OAuth2UserInfo oAuth2UserInfo  = null;
+        if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            System.out.println("구글 로그인 요청~~");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")){
+            System.out.println("네이버 로그인 요청~~");
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response")); //리턴타입이 Map이다.
+        } else {
+            System.out.println("우리는 구글과 네이버만 지원해요 ㅎㅎ");
+        }
+        
+        String provider = oAuth2UserInfo.getProvider(); //google
+        String providerId = oAuth2UserInfo.getProviderId();
         String username = provider+"_"+providerId;
         String password =bCryptPasswordEncoder.encode("임시");
-        String email = oAuth2User.getAttribute("email");
-        String email = oAuth2User.getAttribute("email");
+       // String email = oAuth2User.getAttribute("email");
+        String email = oAuth2UserInfo.getEmail();
         String role = "ROLE_USER";
 
+
         //3-1.
+        //System.out.println("oAuth2UserInfo.getProvider() : " + oAuth2UserInfo.getProvider());
         User userEntity = userRepository.findByUsername(username);
 
         //회원정보가 없을 때 > 회원가입 진행
+        User user;
         if(userEntity == null){
-            System.out.println("구글로그인이 최초입니다.");
+            System.out.println("처음 로그인입니다.");
             userEntity = User.builder()
                     .username(username)
                     .password(password)
@@ -59,7 +79,10 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
                     .build();
             userRepository.save(userEntity);
         }else{
-            System.out.println("구글로그인을 진행한 적입습니다. 자동 회원가입이 진행됩니다.");
+            // user가 존재하면 update 해주기
+            userEntity.setEmail(email);
+            userRepository.save(userEntity);
+            System.out.println("로그인을 진행한 적이 있습니다. 자동 회원가입이 진행됩니다.");
         }
 
         return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
